031d143a2a6b55bd1747f151407f016cccf30afa
Abderrahim Oussayh <abderrahim.oussayh@um6p.ma>
2025-05-12 00:04:14 +0100

    hehe

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
     </div>
 
     <!-- Recently Played Section for authenticated users -->
-    <div v-else>
+    <div v-else class="rounded-lg">
       <h1 class="text-3xl font-bold mb-8">Recently Played</h1>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div v-for="item in recentlyPlayed" :key="item.track.id" 
